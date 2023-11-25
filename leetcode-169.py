314923cecc1bb22f8b86abbf3514d33d0f536be6
xiaohong <example@.com>
2023-11-25 22:22:53 +0800

    add

---
+++ leetcode-169.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        res, maxCount = 0, 0
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+            res = n if count[n] > maxCount else res
+            maxCount = max(count[n], maxCount)
+
+        return res
+
+    def majorityElementV2(self, nums: List[int]) -> int:
+        res, count = 0, 0
+        for n in nums:
+            if res == n:
+                count += 1
+            else:
+                count -= 1
+                if count < 0:
+                    res = n
+                    count = 0
+
+        return res
+
+    def majorityElementV21(self, nums: List[int]) -> int:
+        res, count = 0, 0
+        for n in nums:
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
+
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [3, 2, 3]
+    print(s.majorityElement(nums))
--- leetcode-28.py
+++ leetcode-28.py
@@ -26,12 +26,43 @@ class Solution:
         return -1
 
     def strStrKMP(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        # lps数组每个元素lps[i]表示模式字符串P从索引i开始的最长前缀,也即P[0..i-1]与P[i..P.length()-1]最长相同部分的长度。
+        lps = [0] * len(needle)
+        prevLPS, i = 0, 1
+        while i < len(needle):
+            if needle[i] == needle[prevLPS]:
+                lps[i] = prevLPS + 1
+                prevLPS += 1
+                i += 1
+            else:
+                if prevLPS == 0:
+                    lps[i] = 0
+                    i += 1
+                else:
+                    prevLPS = lps[prevLPS - 1]
+
+        print(lps)  # 需要看看lps数组的定义
+
+        i = 0  # ptr for haystack
+        j = 0  # ptr for needle
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                i, j = i + 1, j + 1
+            else:
+                if j == 0:
+                    i += 1
+                else:
+                    j = lps[j - 1]
+            if j == len(needle):
+                return i - len(needle)
         return 0
 
 
 if __name__ == '__main__':
     haystack = "sadbutsad"
-    needle = "sad"
+    needle = "adbut"
     s = Solution()
-    a = s.strStr(haystack, needle)
+    a = s.strStrKMP(haystack, needle)
     print(a)
