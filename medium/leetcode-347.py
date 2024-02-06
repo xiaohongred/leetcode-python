aa9bcb7e671926c99ad191d53fc48b4e5e231d3a
xiaohong <15200689849@163.com>
2024-02-06 21:27:29 +0800

    add

---
+++ medium/leetcode-347.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+        return res
+
+
+if __name__ == '__main__':
+    nums = [1, 1, 1, 2, 2, 3]
+    k = 2
+    s = Solution()
+    a = s.topKFrequent(nums, k)
+    print(a)
