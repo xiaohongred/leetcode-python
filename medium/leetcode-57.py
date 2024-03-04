82aee975f1fbcf40bc241b9e4583ea7a2f2d22ee
xiaohong <example@.com>
2024-03-04 22:30:34 +0800

    add

---
+++ medium/leetcode-57.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]),
+                               max(newInterval[1], intervals[i][1])]
+
+        res.append(newInterval)
+        return res
+
+
+if __name__ == '__main__':
+    intervals = [[1, 3], [6, 9]]
+    newInterval = [2, 5]
+
+    s = Solution()
+    a = s.insert(intervals, newInterval)
+    print(a)
---
+++ medium/leetcode-74.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        ROWS, COLS = m, n
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        if not (top <= bot):
+            return False
+
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if target > matrix[row][mid]:
+                l = mid + 1
+            elif target < matrix[row][mid]:
+                r = mid - 1
+            else:
+                return True
+        return False
+
+
+if __name__ == '__main__':
+    matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
+    target = 3
+    s = Solution()
+    a = s.searchMatrix(matrix, target)
+    print(a)
