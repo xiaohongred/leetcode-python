4f1b70c0f98b772a6a8568f15b4adb6ab092120e
xiaohong <15200689849@163.com>
2024-04-10 23:16:56 +0800

    update comment

--- medium/leetcode-518.py
+++ medium/leetcode-518.py
@@ -24,7 +24,7 @@ class Solution:
         dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
         dp[0] = [1] * (len(coins) + 1)
         for a in range(1, amount + 1):  # a amount 行
-            for i in range(len(coins) - 1, -1, -1):  # i 第i个面值的硬币 coins[i]
+            for i in range(len(coins) - 1, -1, -1):  # i 第i个硬币，面值为coins[i]
                 dp[a][i] = dp[a][i + 1]
                 if a - coins[i] >= 0:
                     dp[a][i] += dp[a - coins[i]][i]
