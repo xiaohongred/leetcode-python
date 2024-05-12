ec9a6a48772003ed2e261a33b64f97129e19d8e2
xiaohong <15200689849@163.com>
2024-05-12 23:23:56 +0800

    add comment

--- medium/leetcode-309.py
+++ medium/leetcode-309.py
@@ -18,12 +18,12 @@ class Solution:
                 return dp[(i, buying)]
 
             if buying:  # 买入或者不操作
-                buy = dfs(i + 1, not buying) - prices[i]
-                cooldown = dfs(i + 1, buying)
+                buy = dfs(i + 1, not buying) - prices[i]  # 第i天买入，第i+1天不能买入
+                cooldown = dfs(i + 1, buying)  # 第i天不操作
                 dp[(i, buying)] = max(buy, cooldown)
             else:  # 卖出或者不操作
                 sell = dfs(i + 2, not buying) + prices[i]  # 第i天卖出， i+2 天才能买入
-                cooldown = dfs(i + 1, buying)
+                cooldown = dfs(i + 1, buying)  # 第i天不操作，第i+1天才做buging操作
                 dp[(i, buying)] = max(sell, cooldown)
             return dp[(i, buying)]
 
