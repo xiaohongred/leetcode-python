75417bd6cf5705974faa736dd3d9ce36dd9ded49
xiaohong <1027959608@qq.com>
2024-06-27 22:32:58 +0800

    add

--- backtracking/leetcode-17.py
+++ backtracking/leetcode-17.py
@@ -29,9 +29,35 @@ class Solution:
         dfs(0)
         return res
 
+    def letterCombinationsV2(self, digits: str) -> List[str]:
+
+        phontTable = {"2": "abc",
+                      "3": "def",
+                      "4": "ghi",
+                      "5": "jkl",
+                      "6": "mno",
+                      "7": "pqrs",
+                      "8": "tuv",
+                      "9": "wxyz"}
+        res = []
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in phontTable[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+        return res
+
 
 if __name__ == '__main__':
     digits = "23"
     s = Solution()
     a = s.letterCombinations(digits)
+    print(a)
+
+    a = s.letterCombinationsV2(digits)
     print(a)
