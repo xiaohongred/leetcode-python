412ffb4b6018d2a77695d7ee40b6299867817751
unknown <1027959608@qq.com>
2024-07-14 11:04:05 +0800

    add

--- dynamic-programming/leetcode-518.py
+++ dynamic-programming/leetcode-518.py
@@ -35,6 +35,22 @@ class Solution:
                     dp[a][i] += dp[a - coins[i]][i]  # include skip coins[i]
         return dp[amount][0]
 
+    def changeV3(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for i in range(len(coins) - 1, -1, -1):
+            nextDP = [0] * (amount + 1)
+            nextDP[0] = 1
+
+            for a in range(1, amount + 1):
+                nextDP[a] = dp[a]
+                if a - coins[i] >= 0:
+                    nextDP[a] += nextDP[a - coins[i]]
+
+            dp = nextDP
+        return dp[amount]
+
 
 if __name__ == '__main__':
     s = Solution()
@@ -44,4 +60,7 @@ if __name__ == '__main__':
     print(a)
 
     a = s.changeV2(amount, coins)
+    print(a)
+
+    a = s.changeV3(amount, coins)
     print(a)
