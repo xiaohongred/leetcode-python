d4e23fde58784c99f38e2c8aa26910fdb25b1d6e
xiaohongred <1027959608@qq.com>
2024-07-21 11:51:29 +0800

    add

--- dynamic-programming/leetcode-322.py
+++ dynamic-programming/leetcode-322.py
@@ -13,6 +13,31 @@ class Solution:
 
         return dp[amount] if dp[amount] != (amount + 1) else -1
 
+    def coinChangeDFS(self, coins: List[int], amount: int) -> int:
+        cache = {}
+        coins.sort()
+
+        def dfs(amount):
+            if amount == 0:
+                return 0
+            if amount in cache:
+                return cache[amount]
+
+            min_coins = float('inf')
+            for c in coins:
+                if amount - c >= 0:
+                    sub = dfs(amount - c)
+                    if sub != -1:
+                        min_coins = min(min_coins, 1 + sub)
+                else:
+                    break
+
+            cache[amount] = min_coins if min_coins != float('inf') else -1
+            return cache[amount]
+
+        res = dfs(amount)
+        return res
+
 
 if __name__ == '__main__':
     coins = [1, 2, 5]
@@ -25,4 +50,14 @@ if __name__ == '__main__':
     coins = [2]
     amount = 3
     a = s.coinChange(coins, amount)
+    print(a)
+
+    coins = [1, 2, 5]
+    amount = 11
+    a = s.coinChangeDFS(coins, amount)
+    print(a)
+
+    coins = [2]
+    amount = 3
+    a = s.coinChangeDFS(coins, amount)
     print(a)
