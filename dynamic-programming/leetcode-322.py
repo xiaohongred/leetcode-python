c8f6aa9788f589ac940812043f7a6221a35d6fb6
xiaohong <1027959608@qq.com>
2024-07-21 11:14:28 +0800

    add

---
+++ dynamic-programming/leetcode-322.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+
+        return dp[amount] if dp[amount] != (amount + 1) else -1
+
+
+if __name__ == '__main__':
+    coins = [1, 2, 5]
+    amount = 11
+
+    s = Solution()
+    a = s.coinChange(coins, amount)
+    print(a)
+
+    coins = [2]
+    amount = 3
+    a = s.coinChange(coins, amount)
+    print(a)
