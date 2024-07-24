325b5a4f593335723a1c9ea85b995ab45a56d9c3
xiaohong <15200689849@163.com>
2024-07-24 23:09:02 +0800

    add

--- dynamic-programming/leetcode-96.py
+++ dynamic-programming/leetcode-96.py
@@ -1,5 +1,10 @@
 class Solution:
     def numTrees(self, n: int) -> int:
+        
+        # numTree[4] = numTree[0] * numTree[3]
+        #            + numTree[1] * numTree[2]
+        #            + numTree[2] * numTree[1]
+        #            + numTree[3] * numTree[0]
         numTree = [1] * (n + 1)
         # 0 node = 1 tree
         # 1 node = 1 tree
